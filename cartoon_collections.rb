2c6497e97e0526dca1dce681ef2b2984eb205e93
Ryna Truong <rynatruong@outlook.com>
2020-06-23 16:20:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,12 +21,12 @@ def long_planeteer_calls(arr_calls)
 end
 
 def find_the_cheese(cheese_arr)
+    cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_arr.each do |cheeses|
-    if cheeses.include?(cheese_types)
+    if cheeses.include?(cheese_types[i])
       return cheese_types
     else
       return nil 
     end
   end
 end
-  cheese_types = ["cheddar", "gouda", "camembert"]
